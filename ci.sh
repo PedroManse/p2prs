2be56193e439c60cc2c496ac92cc8e67dcc2628f
Pedro Manse <pedro.manse@dmk3.com.br>
2025-07-18 16:26:16 -0300

    upload

---
+++ ci.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+if [ "$1" = "--allow-dirty" ] || [ "$2" = "--allow-dirty" ] ; then allow_dirty="--allow-dirty" ; fi
+if [ "$1" = "--fix" ] || [ "$2" = "--fix" ] ; then fix="--fix" ; fi
+
+set -ex
+cargo build
+cargo fmt
+cargo clippy $fix $allow_dirty --all-targets --all-features -- \
+	-Dclippy::perf \
+	-Dclippy::style \
+	-Wclippy::pedantic \
+	-Aclippy::unnested_or_patterns \
+	-Aclippy::wildcard_imports \
+	-Aclippy::enum_glob_use \
+	-Aclippy::too_many_lines \
+	-Aclippy::match_same_arms \
+	-Aclippy::unnecessary_wraps \
+	-Aclippy::missing_errors_doc
+cargo test
